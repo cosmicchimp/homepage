51949fda47d33efe85313bb6e6cf98834970028a
cosmicchimp <max.lambert429@gmail.com>
2025-03-03 15:41:11 -0700

    mobile add-ons

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const aboutText = document.querySelector("#aboutText");
 const mediaCheck = () => {
   if (mediaQuery.matches) {
     aboutText.innerHTML =
-      "Hi, my name is CosmicChimp, have a look at some of my work.";
+      "Welcome mobile user, my name is CosmicChimp, have a look at some of my work below.";
   } else {
     aboutText.innerHTML =
-      "Hi, my name is CosmicChimp, I'm an aspiring web developer. I built this webpage entirely using my knowledge of HTML, CSS, and JavaScript.";
+      "Hi, my name is CosmicChimp, I'm an aspiring web developer. I built this webpage entirely using my knowledge of HTML, CSS, and JavaScript. Have a look at some of my work below.";
   }
 };
 window.addEventListener("resize", () => {
