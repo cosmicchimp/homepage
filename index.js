508baf226cde36d7a4ab86e349d1feffae89942a
cosmicchimp <max.lambert429@gmail.com>
2025-03-03 16:17:10 -0700

    mobile add-ons

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//All of this is functionality to check for mobile/smaller screens and it will change the about text accordingly to fit the screen
 const mediaQuery = window.matchMedia("(max-width: 600px)");
 const aboutText = document.querySelector("#aboutText");
+const mobileQuery = window.matchMedia("(max-width:900px)");
 const mediaCheck = () => {
-  if (mediaQuery.matches) {
+  if (mediaQuery.matches && !mobileQuery.matches) {
     aboutText.innerHTML =
-      "Welcome mobile user, my name is CosmicChimp, have a look at some of my work below.";
+      "Hi, my name is CosmicChimp, have a look at some of my work below.";
   } else {
     aboutText.innerHTML =
       "Hi, my name is CosmicChimp, I'm an aspiring web developer. I built this webpage entirely using my knowledge of HTML, CSS, and JavaScript. Have a look at some of my work below.";
@@ -13,3 +15,5 @@ window.addEventListener("resize", () => {
   mediaCheck();
 });
 mediaCheck();
+
+//image carousel functionality
